7d7c70f8481c2949389add8082d624581044fea3
Adelane Junior <adelane.junior13@gmail.com>
2024-06-20 13:47:01 -0300

    Revert "n commitar"
    This reverts commit 2af4fff34e9aa9820959a69bebf3651fed387cfc.

---
+++ matricular_api/lib/src/model/sort_object.g.dart
@@ -0,0 +1,110 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sort_object.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+class _$SortObject extends SortObject {
+  @override
+  final bool? empty;
+  @override
+  final bool? unsorted;
+  @override
+  final bool? sorted;
+
+  factory _$SortObject([void Function(SortObjectBuilder)? updates]) =>
+      (new SortObjectBuilder()..update(updates))._build();
+
+  _$SortObject._({this.empty, this.unsorted, this.sorted}) : super._();
+
+  @override
+  SortObject rebuild(void Function(SortObjectBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  SortObjectBuilder toBuilder() => new SortObjectBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is SortObject &&
+        empty == other.empty &&
+        unsorted == other.unsorted &&
+        sorted == other.sorted;
+  }
+
+  @override
+  int get hashCode {
+    var _$hash = 0;
+    _$hash = $jc(_$hash, empty.hashCode);
+    _$hash = $jc(_$hash, unsorted.hashCode);
+    _$hash = $jc(_$hash, sorted.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper(r'SortObject')
+          ..add('empty', empty)
+          ..add('unsorted', unsorted)
+          ..add('sorted', sorted))
+        .toString();
+  }
+}
+
+class SortObjectBuilder implements Builder<SortObject, SortObjectBuilder> {
+  _$SortObject? _$v;
+
+  bool? _empty;
+  bool? get empty => _$this._empty;
+  set empty(bool? empty) => _$this._empty = empty;
+
+  bool? _unsorted;
+  bool? get unsorted => _$this._unsorted;
+  set unsorted(bool? unsorted) => _$this._unsorted = unsorted;
+
+  bool? _sorted;
+  bool? get sorted => _$this._sorted;
+  set sorted(bool? sorted) => _$this._sorted = sorted;
+
+  SortObjectBuilder() {
+    SortObject._defaults(this);
+  }
+
+  SortObjectBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _empty = $v.empty;
+      _unsorted = $v.unsorted;
+      _sorted = $v.sorted;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(SortObject other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$SortObject;
+  }
+
+  @override
+  void update(void Function(SortObjectBuilder)? updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  SortObject build() => _build();
+
+  _$SortObject _build() {
+    final _$result = _$v ??
+        new _$SortObject._(empty: empty, unsorted: unsorted, sorted: sorted);
+    replace(_$result);
+    return _$result;
+  }
+}
+
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
---
+++ matricular_api/test/usuario_controller_search_fields_action_page_request_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:matricular/matricular.dart';
+
+// tests for UsuarioControllerSearchFieldsActionPageRequest
+void main() {
+  final instance = UsuarioControllerSearchFieldsActionPageRequestBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(UsuarioControllerSearchFieldsActionPageRequest, () {
+    // BuiltList<SearchFieldValue> searchFieldValues
+    test('to test the property `searchFieldValues`', () async {
+      // TODO
+    });
+
+    // Pageable page
+    test('to test the property `page`', () async {
+      // TODO
+    });
+  });
+}
